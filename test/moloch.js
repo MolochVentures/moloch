6c8c3e6f55931dff78eba1d14aacd447eb60a328
Rahul Sethuram <rahul@rahuls-mbp.lan>
2018-02-22 19:00:20 -0800

    Test

--- test/moloch.js
+++ test/moloch.js
@@ -16,7 +16,7 @@ contract('Moloch', accounts => {
     const INITIAL_VOTING_SHARES = 10000
 
     const moloch = await Moloch.deployed()
-    const votingSharesAddr = await moloch.votingShares.call()
+    const votingSharesAddr = await moloch.votingSharesAddr.call()
     const votingShares = await VotingShares.at(votingSharesAddr)
     const balance = await votingShares.balanceOf.call(moloch.address)
 
@@ -26,16 +26,57 @@ contract('Moloch', accounts => {
     )
   })
 
-  it('should submit application with eth', async () => {
-    const ETH_TRIBUTE = web3.toWei(10, 'ether')
+  it('should take founding members', async () => {
+    const TRIBUTE = web3.toWei(10, 'ether')
+    const VOTING_SHARES = 100
+    const simpleToken = await SimpleToken.new({ from: accounts[9] })
+    const FOUNDING_MEMBERS = [
+      {
+        memberAddress: accounts[8],
+        votingShares: VOTING_SHARES,
+        ethTributeAmount: TRIBUTE,
+        tokenTributeAddr: '0x0',
+        tokenTributeAmount: 0
+      },
+      {
+        memberAddress: accounts[9],
+        votingShares: VOTING_SHARES,
+        ethTributeAmount: 0,
+        tokenTributeAddr: simpleToken.address,
+        tokenTributeAmount: TRIBUTE
+      }
+    ]
 
     const moloch = await Moloch.deployed()
-    const result = await moloch.submitApplication(100, 0x0, 0, {
+    const promises = FOUNDING_MEMBERS.map(member => {
+      return moloch.addFoundingMember(
+        member.memberAddress,
+        member.votingShares,
+        member.tokenTributeAddr,
+        member.tokenTributeAmount,
+        { from: accounts[0], value: member.ethTributeAmount }
+      )
+    })
+    const results = await Promise.all(promises)
+    results.forEach((result, index) => {
+      const approved = result.logs.find(log => {
+        return log.event === 'MemberApproved'
+      })
+      assert.equal(approved.memberAddress, FOUNDING_MEMBERS.memberAddress)
+    })
+  })
+
+  it('should submit application with eth', async () => {
+    const ETH_TRIBUTE = web3.toWei(10, 'ether')
+    const VOTING_SHARES = 100
+
+    const moloch = await Moloch.deployed()
+    const result = await moloch.submitApplication(VOTING_SHARES, 0x0, 0, {
       value: ETH_TRIBUTE,
       from: accounts[1]
     })
     const log = result.logs.find(log => {
-      return log.event === 'MemberApplication'
+      return log.event === 'MemberApplied'
     })
 
     assert.equal(log.args.memberAddress, accounts[1])
@@ -44,6 +85,7 @@ contract('Moloch', accounts => {
 
   it('should submit application with tokens', async () => {
     const TOKEN_TRIBUTE = web3.toWei(10, 'ether')
+    const VOTING_SHARES = 100
 
     const simpleToken = await SimpleToken.new({ from: accounts[1] })
     const balance = await simpleToken.balanceOf.call(accounts[1])
@@ -51,7 +93,7 @@ contract('Moloch', accounts => {
 
     const moloch = await Moloch.deployed()
     const result = await moloch.submitApplication(
-      100,
+      VOTING_SHARES,
       simpleToken.address,
       TOKEN_TRIBUTE,
       {
@@ -59,7 +101,7 @@ contract('Moloch', accounts => {
       }
     )
     const log = result.logs.find(log => {
-      return log.event === 'MemberApplication'
+      return log.event === 'MemberApplied'
     })
 
     assert.equal(log.args.tokenTributeAmount.toNumber(), TOKEN_TRIBUTE)
