84fb1d7d3c134c851347db2043ec98d9735b1bff
Rahul Sethuram <rahul@rahuls-mbp.lan>
2018-02-26 18:50:26 -0800

    Add guild bank

---
+++ contracts/GuildBank.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.0;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+
+contract GuildBank is Ownable {
+  address[] public tokensHeld; // array of token contracts that are held by bank. any better way to do this?
+
+  function addToken(address _tokenContractAddress) public onlyOwner {
+    tokensHeld.push(_tokenContractAddress);
+  }
+}
