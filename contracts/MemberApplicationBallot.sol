97e3ee9911cf3b5c64fc5d8ea7c9e3cbcab3cb23
Rahul Sethuram <rahul@rahuls-mbp.lan>
2018-03-07 19:47:23 -0800

    Add functionality from generic voting contract, simplify, document.

--- contracts/MemberApplicationBallot.sol
+++ contracts/MemberApplicationBallot.sol
@@ -1,64 +1,126 @@
 pragma solidity ^0.4.18;
 
-import './Voting.sol';
 import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
 
-contract MemberApplicationBallot is Ownable, Voting {
-  address[] public requiredVoters;
-  uint public voteCompletionTime;
+/**
+    @title Membership Application Voting contract.
+    @notice Creates simple voting structure for voting on membership applications.
+    @dev This contract will be owned by the Moloch contract
+ */
+contract MemberApplicationBallot is Ownable {
+    using SafeMath for uint256;
 
-  // standard ballot with 2 proposals
-  // proposal 0 is against, 1 is for
-  function MemberApplicationBallot(
-    address[] _requiredVoters,
-    uint _voteDurationInSeconds
-  ) Voting(2) public 
-  {
-    requiredVoters = _requiredVoters;
-    voteCompletionTime = now + _voteDurationInSeconds * 1 seconds;
+    /***********************************
+    VARIABLES SET AT CONTRACT DEPLOYMENT
+    ************************************/
+    uint public voteCompletionTime;
 
-    // give right to vote for all provided voters
-    for (uint8 i = 0; i < _requiredVoters.length; i++) {
-      giveRightToVote(_requiredVoters[i]);
-    }
-  }
+    // VOTERSHIP REQUIREMENTS
+    // require half the votes
+    uint8 public fractionOfVotersRequiredNumerator = 1;
+    uint8 public fractionOfVotersRequiredDenominator = 2;
+    uint256 public numVotesRequired;
 
-  // must be onlyOwner since contract will be owned by moloch contract
-  function voteAgainst(address voter) public onlyOwner {
-    proxyVote(voter, 0);
-  }
+    // TODO: should we store candidate's address here
 
-  function voteFor(address voter) public onlyOwner {
-    proxyVote(voter, 1);
-  }
+    /************
+    VOTE TRACKING
+    ************/
 
-  function howManyVoters() public view returns (uint) {
-    return requiredVoters.length;
-  }
-
-  function hasEveryoneVoted() public view returns (bool) {
-    bool yes = true;
-    for (uint8 i = 0; i < requiredVoters.length; i++) {
-      yes = yes && hasVoted(requiredVoters[i]);
+    // VOTER
+    struct Voter {
+        uint weight;
+        bool voted;
+        uint8 vote;
     }
 
-    return yes;
-  }
+    mapping (address => Voter) public voters;
 
-  function isAccepted() public view returns (bool) {
-    require(this.hasEveryoneVoted());
-    require(now > voteCompletionTime);
-  
-    uint8 winner = winningProposal();
-
-    if (winner == 0) {
-      return false;
-    } else if (winner == 1) {
-      return true;
-    } else {
-      // cant get here
-      assert(false);
+    // BALLOT
+    struct Proposal {
+        uint256 voteCount;
     }
-  }
+
+    Proposal[2] public ballot; // two proposals: 0 = against, 1 = for
+
+    /// @notice Constructor sets vote parameters
+    /// @param _totalNumberOfVoters Number of people who can vote
+    function MemberApplicationBallot(
+        uint256 _totalNumberOfVoters,
+        uint256 _voteDurationInSeconds
+    ) 
+        public 
+    {
+        voteCompletionTime = now + _voteDurationInSeconds * 1 seconds;
+
+        // numVoters * numerator / denominator, integer division
+        numVotesRequired = (_totalNumberOfVoters.mul(fractionOfVotersRequiredNumerator)).div(fractionOfVotersRequiredDenominator);
+    }
+
+    /// @notice Gives user the right to vote
+    /// @param _toVoterAddress Voter who should be given the right to vote
+    function giveRightToVote(address _toVoterAddress) public onlyOwner {
+        Voter storage voter = voters[_toVoterAddress];
+        require(!voter.voted);
+
+        // TODO: weighted by tokens
+        voter.weight = 1;
+    }
+
+    /// @notice Vote for candidate
+    /// @param _voterAddress Address of person who is voting
+    /// @param _voteFor Boolean to vote in favor of candidate
+    function vote(address _voterAddress, bool _voteFor) public {
+        Voter storage voter = voters[_voterAddress];
+
+        require(voter.voted = false);
+        voter.voted = true;
+
+        if (_voteFor) {
+            ballot[1].voteCount += voter.weight;
+        } else {
+            ballot[0].voteCount += voter.weight;
+        }
+    }
+
+    /// @notice Get voter attributes
+    /// @param _voterAddress Voter address
+    /// @return Voter attributes
+    function getVoter(address _voterAddress) public view returns(uint, bool, uint8) {
+        Voter memory voter = voters[_voterAddress];
+
+        return (voter.weight, voter.voted, voter.vote);
+    }
+
+    /// @notice Check if enough people have voted
+    /// @return True if enough people have voted, false otherwise
+    function haveEnoughVoted() public view returns (bool) {
+        uint256 totalVotes = ballot[0].voteCount + ballot[1].voteCount;
+        return totalVotes > numVotesRequired;
+    }
+
+    /// @notice Check if vote duration period has elapsed
+    /// @return True if vote time is complete
+    function hasVoteDurationPeriodElapsed() public view returns (bool) {
+        return now > voteCompletionTime;
+    }
+
+    /// @notice Check if candidate is accepted
+    /// @dev Tie = not accepted
+    /// @return True if candidate is accepted, false otherwise
+    function isCandidateAccepted() public view returns (bool) {
+        require(haveEnoughVoted());
+        require(hasVoteDurationPeriodElapsed());
+
+        uint256 votesFor = ballot[1].voteCount;
+        uint256 votesAgainst = ballot[0].voteCount;
+    
+        if (votesFor > votesAgainst) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
