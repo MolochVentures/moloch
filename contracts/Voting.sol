5fb75349033a3b515ecf71998c41cae574efe129
Rahul Sethuram <rahul@rahuls-mbp.lan>
2018-03-12 19:22:45 -0700

    Code review feedback 3/12
    Change to "voting period" instead of minimum time of vote being open.
    Votes must be cast within the period, and cannot be cast when period
    is over.
    
    Voting participation is now determined by voting shares not number of
    people that voted.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -15,14 +15,14 @@ contract Voting is Ownable {
     /***********************************
     VARIABLES SET AT CONTRACT DEPLOYMENT
     ************************************/
-    uint public voteCompletionTime;
+    uint public votingPeriodEnds;
     VotingShares votingShares;
 
     // VOTERSHIP REQUIREMENTS
     // require half the votes
     uint8 public fractionOfVotersRequiredNumerator = 1;
     uint8 public fractionOfVotersRequiredDenominator = 2;
-    uint256 public numVotesRequired;
+    uint256 public numVotesRequired; // number of voting shares required for valid vote
 
     /************
     VOTE TRACKING
@@ -30,53 +30,46 @@ contract Voting is Ownable {
 
     // VOTER
     struct Voter {
-        bool allowedToVote;
-        bool voted;
-        uint8 vote;
+        bool voted; // have they voted
+        uint8 vote; // vote choice as item within proposal
     }
 
     mapping (address => Voter) public voters;
-    uint256 numberOfVoted = 0;
 
     // BALLOT
     struct Proposal {
         uint256 voteCount;
     }
 
-    Proposal[] public proposals; // two proposals: 0 = against, 1 = for
+    Proposal[] public proposals;
 
     /// @notice Constructor sets vote parameters
-    /// @param _totalNumberOfVoters Number of people who can vote
-    /// @param _voteDurationInSeconds How long does vote have to last at minimum
+    /// @param _votingSharesAddress Address of voting shares contract
+    /// @param _votingPeriodLengthInSeconds How long is the voting period open
     /// @param _numberOfProposals How many proposals on the ballot
     function Voting(
-        uint256 _totalNumberOfVoters,
-        uint256 _voteDurationInSeconds,
+        address _votingSharesAddress,
+        uint256 _votingPeriodLengthInSeconds,
         uint8 _numberOfProposals
     ) 
         public 
     {
-        voteCompletionTime = now + _voteDurationInSeconds * 1 seconds;
+        votingShares = VotingShares(_votingSharesAddress);
+        votingPeriodEnds = now + _votingPeriodLengthInSeconds * 1 seconds;
+        uint256 totalVotingShares = votingShares.totalSupply();
 
         // numVoters * numerator / denominator, integer division
-        numVotesRequired = (_totalNumberOfVoters.mul(fractionOfVotersRequiredNumerator)).div(fractionOfVotersRequiredDenominator);
+        numVotesRequired = (totalVotingShares.mul(fractionOfVotersRequiredNumerator)).div(fractionOfVotersRequiredDenominator);
 
         proposals.length = _numberOfProposals;
-    }
-
-    /// @notice Gives user the right to vote
-    /// @param _toVoterAddress Voter who should be given the right to vote
-    function giveRightToVote(address _toVoterAddress) public onlyOwner {
-        Voter storage voter = voters[_toVoterAddress];
-        require(!voter.voted);
-
-        voter.allowedToVote = true;
     }
 
     /// @notice Vote for candidate, number of voting shares = number of votes
     /// @param _voterAddress Address of person who is voting
     /// @param _toProposal Which proposal to vote on
     function vote(address _voterAddress, uint8 _toProposal) public {
+        require(now < votingPeriodEnds); // vote is still open
+
         Voter storage voter = voters[_voterAddress];
 
         require(voter.voted = false);
@@ -85,34 +78,37 @@ contract Voting is Ownable {
         // votes = number of voting shares
         uint256 shares = votingShares.balanceOf(_voterAddress);
         proposals[_toProposal].voteCount += shares;
-        numberOfVoted++;
     }
 
     /// @notice Get voter attributes
     /// @param _voterAddress Voter address
     /// @return Voter attributes
-    function getVoter(address _voterAddress) public view returns(bool, bool, uint8) {
+    function getVoter(address _voterAddress) public view returns(bool, uint8) {
         Voter memory voter = voters[_voterAddress];
 
-        return (voter.allowedToVote, voter.voted, voter.vote);
+        return (voter.voted, voter.vote);
     }
 
     /// @notice Check if enough people have voted
     /// @return True if enough people have voted, false otherwise
     function haveEnoughVoted() public view returns (bool) {
-        return numberOfVoted > numVotesRequired;
+        uint256 totalNumberOfVotes = 0;
+        for (uint8 i = 0; i < proposals.length; i++) {
+            totalNumberOfVotes += proposals[i].voteCount;
+        }
+        return totalNumberOfVotes > numVotesRequired;
     }
 
     /// @notice Check if vote duration period has elapsed
     /// @return True if vote time is complete
     function hasVoteDurationPeriodElapsed() public view returns (bool) {
-        return now > voteCompletionTime;
+        return now > votingPeriodEnds;
     }
 
-    /// @notice Check if candidate is accepted
-    /// @dev Tie = not accepted
-    /// @return True if candidate is accepted, false otherwise
-    function getWinningProposal() public view returns (uint8) {
+    /// @notice Get the proposal that is currently leading the vote
+    /// @dev Tie = leading proposal set to later index in the array
+    /// @return uint8 of index in array
+    function getLeadingProposal() public view returns (uint8) {
         uint8 winningProposal;
         uint256 winningVoteCount = 0;
         for (uint8 i = 0; i < proposals.length; i++) {
@@ -124,10 +120,13 @@ contract Voting is Ownable {
         return winningProposal;
     }
 
-    function endOfVoteWinner() public view returns (uint8) {
+    /// @notice Get the proposal that won, assuming the vote period is complete
+    /// and enough votes have been cast
+    /// @return uint8 of index in array
+    function getWinnerProposal() public view returns (uint8) {
         require(haveEnoughVoted());
         require(hasVoteDurationPeriodElapsed());
-        return getWinningProposal();
+        return getWinnerProposal();
     }
 }
 
