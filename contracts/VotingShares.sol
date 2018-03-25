1d164bb9602da37a7aa14954cf491d934ed17c4b
Rahul <rksethuram9@gmail.com>
2018-03-25 11:18:53 -0700

    Change to 4 space indent

--- contracts/VotingShares.sol
+++ contracts/VotingShares.sol
@@ -1,81 +1,81 @@
 pragma solidity ^0.4.18;
 
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 /**
  * Structured like an ERC20, but can only be burned/minted and not transferred.
  */
 contract VotingShares is Ownable {
-  using SafeMath for uint256;
+    using SafeMath for uint256;
 
-  uint256 totalSupply_;
-  mapping(address => uint256) balances;
-  bool public mintingFinished = false;
+    uint256 totalSupply_;
+    mapping(address => uint256) balances;
+    bool public mintingFinished = false;
 
-  event Mint(address indexed to, uint256 amount);
-  event Burn(address indexed from, uint256 amount);
-  event Transfer(address indexed from, address indexed to, uint256 value);
-  
-  event MintFinished();
+    event Mint(address indexed to, uint256 amount);
+    event Burn(address indexed from, uint256 amount);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    event MintFinished();
 
-  modifier canMint() {
-    require(!mintingFinished);
-    _;
-  }
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
 
-  /**
-  * @dev total number of tokens in existence
-  */
-  function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-  }
+    /**
+    * @dev total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
 
-  /**
-   * @dev Burns a specific amount of tokens.
-   * @param _burner Who to burn tokens from.
-   * @param _value The amount of token to be burned.
-   */
-  function proxyBurn(address _burner, uint256 _value) public onlyOwner {
-    require(_value <= balances[_burner]);
-    // no need to require value <= totalSupply, since that would imply the
-    // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _burner Who to burn tokens from.
+     * @param _value The amount of token to be burned.
+     */
+    function proxyBurn(address _burner, uint256 _value) public onlyOwner {
+        require(_value <= balances[_burner]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
 
-    balances[_burner] = balances[_burner].sub(_value);
-    totalSupply_ = totalSupply_.sub(_value);
-    Burn(_burner, _value);
-  }
+        balances[_burner] = balances[_burner].sub(_value);
+        totalSupply_ = totalSupply_.sub(_value);
+        Burn(_burner, _value);
+    }
 
-  /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
-  function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
-  }
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 
-  /**
-   * @dev Function to mint tokens
-   * @param _to The address that will receive the minted tokens.
-   * @param _amount The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
-    totalSupply_ = totalSupply_.add(_amount);
-    balances[_to] = balances[_to].add(_amount);
-    Mint(_to, _amount);
-    Transfer(address(0), _to, _amount);
-    return true;
-  }
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+        totalSupply_ = totalSupply_.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Mint(_to, _amount);
+        Transfer(address(0), _to, _amount);
+        return true;
+    }
 
-  /**
-   * @dev Function to stop minting new tokens.
-   * @return True if the operation was successful.
-   */
-  function finishMinting() onlyOwner canMint public returns (bool) {
-    mintingFinished = true;
-    MintFinished();
-    return true;
-  }
+    /**
+     * @dev Function to stop minting new tokens.
+     * @return True if the operation was successful.
+     */
+    function finishMinting() onlyOwner canMint public returns (bool) {
+        mintingFinished = true;
+        MintFinished();
+        return true;
+    }
 }
