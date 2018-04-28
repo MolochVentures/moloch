6881ebbb21f936002e6b822bb74767f952f2f6ce
James Young <james.s.young@gmail.com>
2018-04-28 10:32:15 -0700

    foundingMembers as deployment parameters

--- contracts/Moloch.sol
+++ contracts/Moloch.sol
@@ -57,13 +57,29 @@ contract Moloch is Ownable {
     constructor(
         address _votingSharesAddress,
         address _lootTokenAddress,
-        address _guildBankAddress
+        address _guildBankAddress,
+        address[] _membersArray,
+        uint[] _sharesArray
     ) 
         public 
     {
         votingShares = VotingShares(_votingSharesAddress);
         lootToken = LootToken(_lootTokenAddress);
         guildBank = GuildBank(_guildBankAddress);
+
+        require(_membersArray.length == _sharesArray.length);
+
+        for (uint i = 0; i < _membersArray.length; i++) {
+
+            address founder = _membersArray[i];
+            uint founderShares =  _sharesArray[i];
+
+            members.approved[founder] = true;
+            votingShares.mint(msg.sender, founder, founderShares);
+            lootToken.mint(msg.sender, guildBank, founderShares);
+
+            emit MemberAccepted(founder);
+        }
     }
 
     /*****************
@@ -84,7 +100,8 @@ contract Moloch is Ownable {
             msg.value,
             _tokenTributeAddresses, 
             _tokenTributeAmounts,
-            _votingSharesRequested
+            _votingSharesRequested,
+            guildBank
         );
     }
 
@@ -117,10 +134,6 @@ contract Moloch is Ownable {
 
     function finishProposal() public onlyApprovedMember {
         proposalQueue.finishProposal(members, guildBank, votingShares, lootToken);
-    }
-
-    function addFoundingMember(address _memberAddress, uint256 _votingSharesToGrant) public onlyOwner {
-        members.addFoundingMember(votingShares, lootToken, _memberAddress, _votingSharesToGrant);
     }
 
     /**************
--- migrations/2_moloch_migration.js
+++ migrations/2_moloch_migration.js
@@ -8,8 +8,10 @@ const TownHallLib = artifacts.require('./TownHallLib.sol')
 const VotingLib = artifacts.require('./VotingLib.sol')
 
 module.exports = (deployer, network, accounts) => {
-  deployer
-    .deploy([VotingShares, VotingLib])
+  deployer.deploy(VotingShares)
+    .then(() => {
+      return deployer.deploy(VotingLib)
+    })
     .then(() => {
       deployer.link(VotingLib, TownHallLib)
       return deployer.deploy(TownHallLib)
@@ -26,7 +28,9 @@ module.exports = (deployer, network, accounts) => {
         Moloch,
         VotingShares.address,
         LootToken.address,
-        GuildBank.address
+        GuildBank.address,
+        ["0xb93d509ee94531c17695c315e833a133b014e5e6", "0x789c38380d8c431b3ab70cb578b6566e8e4f6684"],
+        [123, 456]
       )
     })
 }
