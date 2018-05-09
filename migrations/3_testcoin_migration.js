c42c89d76b939b8724a699efcc4d9d165c9219c9
Rahul <rksethuram9@gmail.com>
2018-05-08 19:37:38 -0700

    Deployment issues

--- migrations/3_testcoin_migration.js
+++ migrations/3_testcoin_migration.js
@@ -4,9 +4,9 @@ const TestCoin = artifacts.require('./TestCoin.sol')
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
-    let testJSON = {addresses:[]}
-    for (let i=0; i < 3; i++) {
-      await deployer.deploy(TestCoin)
+    let testJSON = { addresses: [] }
+    for (let i = 0; i < 3; i++) {
+      await deployer.deploy(TestCoin, { from: accounts[8] })
       testJSON.addresses.push(TestCoin.address)
     }
     await fse.writeJson('./test/testcoins.json', testJSON)
