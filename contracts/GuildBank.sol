8e1d59a1684eee65ee10f156b6b8bd99cd26a794
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2019-11-09 06:39:29 -0500

    Update GuildBank.sol
    Minor cleanup changes

--- contracts/GuildBank.sol
+++ contracts/GuildBank.sol
@@ -9,8 +9,8 @@ contract GuildBank is Ownable {
 
     event Withdrawal(address indexed receiver, uint256 amount);
 
-    function withdraw(address receiver, uint256 shares, uint256 totalShares, IERC20[] approvedTokens) public onlyOwner returns (bool) {
-        for (var i=0; i < approvedTokens.length; i++) {
+    function withdraw(address receiver, uint256 shares, uint256 totalShares, IERC20[] memory approvedTokens) public onlyOwner returns (bool) {
+        for (uint256 i=0; i < approvedTokens.length; i++) {
             uint256 amount = approvedTokens[i].balanceOf(address(this)).mul(shares).div(totalShares);
             emit Withdrawal(receiver, amount);
             return approvedTokens[i].transfer(receiver, amount);
