6c01ea0f772ead690401898bbcf8b5e2f11e539a
James Morgan <james.morgan.e@googlemail.com>
2019-11-18 10:27:20 +0000

    WIP hello world test

---
+++ test/v2/moloch.js
@@ -0,0 +1,19 @@
+// v2 test spec
+
+const {artifacts, ethereum, web3} = require('@nomiclabs/buidler');
+const chai = require('chai');
+const {assert} = chai;
+
+const BN = web3.utils.BN;
+
+chai
+    .use(require('chai-as-promised'))
+    .should();
+
+contract.only('Moloch V2', ([creator, summoner, applicant1, applicant2, processor, delegateKey, ...otherAccounts]) => {
+
+    it('hello world', async () => {
+        assert.isTrue(true, 'This should be true!');
+    });
+
+});
