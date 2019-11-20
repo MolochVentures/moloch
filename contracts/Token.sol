6eccf01e2ea2a03388915da51b33b5a8fb3c1e09
Vincent de Almeida <vince0656@googlemail.com>
2019-11-20 11:48:53 +0000

    Extended Token to allow transfers to be disabled where required

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,7 +3,18 @@ pragma solidity ^0.5.2;
 import "./oz/ERC20.sol";
 
 contract Token is ERC20 {
-  constructor(uint256 supply) public {
-    _mint(msg.sender, supply);
-  }
+    bool transfersEnabled = true;
+
+    constructor(uint256 supply) public {
+        _mint(msg.sender, supply);
+    }
+
+    function updateTransfersEnabled(bool enabled) external {
+        transfersEnabled = enabled;
+    }
+
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(transfersEnabled);
+        return super.transfer(to, value);
+    }
 }
