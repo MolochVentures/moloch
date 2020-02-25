94dbcfc610e4d723e8c978c372b8be617c4fa5d4
Rahul Sethuram <rahul@Rahuls-MacBook-Pro.local>
2020-02-25 09:51:50 +0100

    Lint + buidler

--- buidler.config.js
+++ buidler.config.js
@@ -1,24 +1,28 @@
-usePlugin('@nomiclabs/buidler-truffle5')
+const buidler = require("@nomiclabs/buidler/config");
+const { usePlugin } = buidler;
 
-require('./scripts/moloch-tasks')
-require('./scripts/pool-tasks')
+usePlugin("@nomiclabs/buidler-etherscan");
+usePlugin("@nomiclabs/buidler-truffle5");
 
-const INFURA_API_KEY = ''
-const MAINNET_PRIVATE_KEY = ''
-const ROPSTEN_PRIVATE_KEY = ''
+require("./scripts/moloch-tasks");
+require("./scripts/pool-tasks");
+
+const INFURA_API_KEY = "";
+const MAINNET_PRIVATE_KEY = "";
+const ROPSTEN_PRIVATE_KEY = "";
 
 module.exports = {
   networks: {
     develop: {
-      url: 'http://localhost:8545',
+      url: "http://localhost:8545",
       deployedContracts: {
-        moloch: '',
-        pool: ''
+        moloch: "",
+        pool: ""
       }
     },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [ ROPSTEN_PRIVATE_KEY ],
+      accounts: [ROPSTEN_PRIVATE_KEY],
       deployedContracts: {
         moloch: "",
         pool: ""
@@ -26,21 +30,29 @@ module.exports = {
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [ MAINNET_PRIVATE_KEY ],
+      accounts: [MAINNET_PRIVATE_KEY],
       deployedContracts: {
-        moloch: '0x1fd169A4f5c59ACf79d0Fd5d91D1201EF1Bce9f1', // The original Moloch
+        moloch: "0x1fd169A4f5c59ACf79d0Fd5d91D1201EF1Bce9f1", // The original Moloch
         pool: ""
       }
     },
     coverage: {
-      url: 'http://localhost:8555'
+      url: "http://localhost:8555"
     }
   },
   solc: {
-    version: '0.5.3',
-    evmVersion: 'constantinople',
+    version: "0.5.3",
+    evmVersion: "constantinople",
     optimizer: {
       enabled: true
     }
+  },
+  etherscan: {
+    // The url for the Etherscan API you want to use.
+    // For example, here we're using the one for the Ropsten test network
+    url: "https://api.etherscan.io/api",
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "2ZAZ2R4NX6Z7522TCTTIIVITV145SWTW63"
   }
-}
+};
